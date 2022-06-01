ed5337cf5ff06625626deb4cb59d3c76c88b210a
Keerthana Yellapragada <keerthanay@gmail.com>
2022-06-01 13:42:04 -0700

    manager

---
+++ manager.js
@@ -0,0 +1,16 @@
+const Employee = require('./employee');
+
+
+class Manager extends Employee {
+    constructor(name, salary, title, manager, employees = []) {
+        super(name, salary, title, manager = null)
+        this.employees = employees
+    }
+
+    addEmployee(employee) {
+        //add to employees array
+        this.employees.push(employee)
+    }
+}
+
+module.exports = Manager;
